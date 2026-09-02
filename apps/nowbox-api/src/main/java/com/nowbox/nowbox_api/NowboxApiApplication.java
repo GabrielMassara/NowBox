309e020ba53c16ba304792412d839725e7226463
Gabriel Massara <75136519+GabrielMassara@users.noreply.github.com>
2026-09-02 16:11:04 -0300

    chore(api): initialize Spring Boot API structure (#2)

---
+++ apps/nowbox-api/src/main/java/com/nowbox/nowbox_api/NowboxApiApplication.java
@@ -0,0 +1,13 @@
+package com.nowbox.nowbox_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowboxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowboxApiApplication.class, args);
+	}
+
+}
