a77fdd5c3cc1b360f71c349022c5a91eea13bfe0
Gabriel Massara <75136519+GabrielMassara@users.noreply.github.com>
2026-09-02 16:00:21 -0300

    chore(api): initialize Spring Boot API structure

---
+++ apps/nowbox-api/src/test/java/com/nowbox/nowbox_api/NowboxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowbox.nowbox_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowboxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
